b5d6157f73451871bfeceae34e6a11706cc9baa0
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-07-30 12:09:24 +0100

    tailwindcss added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Voting dApp</title>
         <meta name="description" content="Voting dApp on Polygon" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>Dummy Voting dApp Page</main>
-    </div>
+    </>
   );
 }
