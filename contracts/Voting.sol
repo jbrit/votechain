fd480ab2b151b678efd333ff2b3672af9aa89b2c
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-08-01 23:56:20 +0100

    voting contract updated

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -8,6 +8,11 @@ contract Voting {
     // structs
     struct Poll {
         address owner;
+        string name;
+        string description;
+        uint start_time;
+        uint end_time;
+        uint fee;
     }
     
     // Variables
@@ -24,15 +29,23 @@ contract Voting {
     uint pollId;
     
     // events
-    event createPollEvt(uint pollId);
+    event createPollEvt(uint pollId, address owner);
 
     constructor() {
         owner = msg.sender;
     }
 
+    // Public Functions 
     function createPoll() public {
-        emit createPollEvt(pollId);
+        emit createPollEvt(pollId, msg.sender);
+        polls[pollId] = Poll(msg.sender, "Default Name", "Default Description", 0, 100, 0);
         pollId += 1;
         console.log("New pollId is '%s'", pollId);
+    }
+
+    function createUser(string memory username) public{
+        // Make checks to ensure user name is not empty && not taken && address not registered
+        address_to_username[msg.sender] = username;
+        username_to_address[username] = msg.sender;
     }
 }
--- test/voter-test.js
+++ test/voter-test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 
 describe("Voting", function () {
-  it("Should have no return value to create a poll", async function () {
+  it("should create a poll with the sender as owner", async function () {
     const Voting = await ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
     await voting.deployed();
