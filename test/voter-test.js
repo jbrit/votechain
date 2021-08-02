c9b3664a625dd6e64794d078e729eacab125cbfd
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-08-02 01:22:59 +0100

    added get polls function

--- test/voter-test.js
+++ test/voter-test.js
@@ -1,11 +1,17 @@
 const { expect } = require("chai");
 
 describe("Voting", function () {
-  it("should create a poll with the sender as owner", async function () {
+  it("should create a polland enable fetching", async function () {
     const Voting = await ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
     await voting.deployed();
 
-    expect(await voting.createPoll()).to.not.equal("Return Value");
+    await voting.createPoll();
+    await voting.createPoll();
+
+    expect((await voting.getPolls())[0]).to.eql([
+      "Default Name",
+      "Default Name",
+    ]);
   });
 });
